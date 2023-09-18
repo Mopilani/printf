22264dc08ce7d83b89941310bc4860f75907cf48
Mohammed Abderrahim <mopilani1@gmail.com>
2023-09-18 05:24:31 -0700

    Add printf.c

--- _printf.c
+++ _printf.c
@@ -1,43 +1,40 @@
 #include "main.h"
 /**
- * _printf - printf function
- * @format: const char pointer
- * Return: b_len
+ * _printf - Custom printf function supporting %c and %s format specifiers.
+ * @format: The format string.
+ * Return: The total number of characters printed.
  */
-int _printf(const char *format, ...)
+int _printf(const char *const format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+	char *str;
+	int _length_ = 0;
+	va_list _list_; /* Initialize a variable argument list */
+	const char *fmt_ptr = format;
 
-	register int count = 0;
-
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
+	va_start(_list_, format); /* Start processing the variable arguments */
+	if (format)
 	{
-		if (*p == '%')
+		while (*fmt_ptr)
 		{
-			p++;
-			if (*p == '%')
+			if (*fmt_ptr == '%')
 			{
-				count += _putchar('%');
-				continue;
+				fmt_ptr++;
+				if (*fmt_ptr == 'c')	/* Handle character format specifier */
+					_length_ += _putchar_(va_arg(_list_, int));
+				else if (*fmt_ptr == 's') /* Handle string format specifier */
+				{
+					_str_ = va_arg(_list_, char *);
+					_length_ += _print_str_(_str_);
+				}
 			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
+			else
+			{ /* Print a character not part of a format specifier */
+				_putchar_(*fmt_ptr);
+				_length_++;
+			}
+			fmt_ptr++;
+		}
 	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+	va_end(_list_); /* End processing of variable arguments */
+	return (_length_); /* Return the total number of characters printed */
 }
