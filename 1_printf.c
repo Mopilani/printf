fc4fcdbf4858059115a488d4a5b5296e96c3fed0
Mohammed Abderrahim <mopilani1@gmail.com>
2023-09-20 16:50:57 -0700

    Hot fix

--- 1_printf.c
+++ 1_printf.c
@@ -9,41 +9,43 @@ int _printf(const char *const format, ...)
 {
 	int _length = 0;
 	va_list _list;
-	const char *_fmt;
+	const char *_fmt_char;
 
 	if (format == NULL)
 		return (-1);
-	_fmt = format;
-	if ((_fmt[0] == '%' && !_fmt[1]) || !_fmt)
+	_fmt_char = format;
+	if ((_fmt_char[0] == '%' && !_fmt_char[1]) || !_fmt_char)
 		return (-1);
-	if ((!_fmt[2] && _fmt[0] == '%' && _fmt[1] == ' '))
+	if ((!_fmt_char[2] && _fmt_char[0] == '%' && _fmt_char[1] == ' '))
 		return (-1);
 
 
 	va_start(_list, format);
 
-	for (; *_fmt; _fmt++)
+	for (; *_fmt_char; _fmt_char++)
 	{
-		if (*_fmt == '%')
+		if (*_fmt_char == '%')
 		{
-			_fmt++; /* Move to the character after '%' */
-			if (*_fmt == 'c' || *_fmt == 's' || *_fmt == 'd' ||
-			*_fmt == 'i' || *_fmt == 'u' ||  *_fmt == 'o' ||  *_fmt == 'b')
-				_length += _slt_func(*_fmt)(_list);
-			else if (*_fmt == '%')
+			_fmt_char++; /* Move to the character after '%' */
+			if (*_fmt_char == 'c' || *_fmt_char == 's' || *_fmt_char == 'd' ||
+			*_fmt_char == 'x' || *_fmt_char == 'X' || *_fmt_char == 'i' || 
+			*_fmt_char == 'u' ||  *_fmt_char == 'o' ||  *_fmt_char == 'b')
+				_length += _slt_func(*_fmt_char)(_list);
+			else if (*_fmt_char == '%')
 			{
 				_length += _putchar('%');
 			}
 			else
 			{
 				_length += _putchar('%');
-				_length += _putchar(*_fmt);
+				_length += _putchar(*_fmt_char);
 				continue;
 			}
 		}
 		else
-			_length += _putchar(*_fmt);
+			_length += _putchar(*_fmt_char);
 	}
 	va_end(_list);
 	return (_length);
 }
+
